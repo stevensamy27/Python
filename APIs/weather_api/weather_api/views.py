5c373ba2146dc6845fb797ce6f29c56015bacc42
Steven Samy <stevensivo27@gmail.com>
2021-11-17 17:35:19 +0200

    weather page

--- APIs/weather_api/weather_api/views.py
+++ APIs/weather_api/weather_api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
+
+def index(request):
+    api_url = 'http://api.openweathermap.org./data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q='
+    city_name = "Kuwait"
+
+    url = api_url + city_name
+    response = requests.get(url)
+    content = response.json()
+
+    city_weather ={
+
+        'city' : city_name,
+        'tempreature' : content['main']['temp'],
+        'descreption':content['weahter'][0]['descreption'],
+        'icon':content['weahter'][0]['icon']
+
+    }
+
+    return render(request, 'weather.html', city_weather)
