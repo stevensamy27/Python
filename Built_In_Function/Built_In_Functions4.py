99bf44642528f99e6aea3d551d159061bdae8258
Steven Samy <stevensivo27@gmail.com>
2021-12-07 15:19:43 +0200

    map( Function  , *Iterator  )

---
+++ Built_In_Function/Built_In_Functions4.py
@@ -0,0 +1,53 @@
+# -------------------------------
+# -- Built In Functions => Map --
+# -------------------------------
+# [1] Map Take A Function + Iterator
+# [2] Map Called Map Because It Map The Function On Every Element
+# [3] The Function Can Be Pre-Defined Function or Lambda Function
+# ---------------------------------------------------------------
+
+# Use Map With Predefined Function
+
+def formatText(text):
+
+  return f"- {text.strip().capitalize()} -"
+
+myTexts = [" steven ", "samy", "  mekhael  "]
+
+myFormatedData = map(formatText, myTexts)
+
+print(myFormatedData)
+
+for name in list(map(formatText, myTexts)):
+
+  print(name)
+
+print("#" * 50)
+
+# Use Map With Lambda Function
+
+def formatText(text):
+
+  return f"- {text.strip().capitalize()} -"
+
+myTexts = [" steven ", "samy", "  mekhael  "]
+
+for name in list(map((lambda text: f"- {text.strip().capitalize()} -"), myTexts)):
+
+  print(name)
+
+
+  '''
+# Use Map With Predefined Function
+- Steven -
+- Samy -
+- Mekhael -
+
+##################################################
+
+# Use Map With Lambda Function
+- Steven -
+- Samy -
+- Mekhael -
+  
+  '''
