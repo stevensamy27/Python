e3fe464254391b999b3a2a5644ed3a29402cfcb9
Steven Samy <stevensivo27@gmail.com>
2021-12-21 23:36:02 +0200

    -- Generators --    'yield'

---
+++ Generators.py
@@ -0,0 +1,36 @@
+# ----------------
+# -- Generators --
+# ----------------
+# [1] Generator is a Function With "yield" Keyword Instead of "return"
+# [2] It Support Iteration and Return Generator Iterator By Calling "yield"
+# [3] Generator Function Can Have one or More "yield"
+# [4] By Using next() It Resume From Where It Called "yield" Not From Begining
+# [5] When Called, Its Not Start Automatically, Its Only Give You The Control
+# -----------------------------------------------------------------
+
+def myGenerator():
+  yield 1
+  yield 2
+  yield 3
+  yield 4
+
+myGen = myGenerator()
+
+print(next(myGen), end="--")
+print("Hello From Python")
+print(next(myGen))
+print("#" * 50)
+
+for number in myGen: # the loop start from last stop on myGenerator (line 21 >> 2)
+  print(number)
+
+
+
+###OUTPUT####
+  '''
+1--Hello From Python
+2
+##################################################
+3
+4
+  '''
