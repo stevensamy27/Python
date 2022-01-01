4dae31895c7d0809a0ad687f9b80a24339d607dc
Steven Samy <stevensivo27@gmail.com>
2022-01-01 22:37:21 +0200

    -- Type Hinting --

---
+++ Type_Hinting.py
@@ -0,0 +1,24 @@
+# ------------------
+# -- Type Hinting --
+# ------------------
+
+def say_hello(name) -> str:
+
+  print(f"Hello {name}")
+
+say_hello("Ahmed")
+
+def calculate(n1, n2) -> str:
+
+  print(n1 + n2)
+
+calculate(10, 40)
+
+
+###OUTPUT###
+
+'''
+Hello Ahmed
+50
+
+'''
